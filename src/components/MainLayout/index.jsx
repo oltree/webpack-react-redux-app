5a197dfb54a2e769b5646761734e3955e2f9f159
oltree <o.meleh10@gmail.com>
2022-12-08 00:03:11 +0300

    added redux

---
+++ src/components/MainLayout/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+const MainLayout = () => {
+  return <div className={styles.wrapper}>MainLayout</div>;
+};
+
+export default MainLayout;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import App from "./components/App.jsx";
+import MainLayout from "./components/MainLayout/index.jsx";
 
-import "./index.scss";
+import { configureStore } from "./store/configureStore";
+
+import "./static/styles/normalize.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = configureStore();
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <MainLayout />
+    </Provider>
   </React.StrictMode>
 );
